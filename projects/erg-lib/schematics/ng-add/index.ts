f02bec045aa5246cb40e4fa7aab4d5a4506fc354
Furkan Ergüldürenler <furkan.erguldurenler@larinsoft.com>
2022-09-02 11:31:08 +0300

    adding bootstrap.css in to angular.json

--- projects/erg-lib/schematics/ng-add/index.ts
+++ projects/erg-lib/schematics/ng-add/index.ts
@@ -1,5 +1,5 @@
 import {Rule, Tree, SchematicContext, SchematicsException} from '@angular-devkit/schematics'
-import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks'
+import {NodePackageInstallTask,RunSchematicTask} from '@angular-devkit/schematics/tasks'
 import {applyToUpdateRecorder} from '@schematics/angular/utility/change';
 import {addImportToModule} from '@schematics/angular/utility/ast-utils';
 import * as ts from "typescript"
@@ -27,7 +27,7 @@ export function ngAdd(): Rule {
       true
     );
 
-
+    context.logger.info('Adding imports to Appmodule.ts')
     applyToUpdateRecorder(recorder,
       addImportToModule(source, modulePath, 'ErgLibModule', 'erg-lib')
     );
@@ -35,15 +35,23 @@ export function ngAdd(): Rule {
     applyToUpdateRecorder(recorder,
       addImportToModule(source, modulePath, 'ReactiveFormsModule', '@angular/forms')
     );
+    applyToUpdateRecorder(recorder,
+      addImportToModule(source, modulePath, 'NgbModule', '@ng-bootstrap/ng-bootstrap')
+    );
+
 
     tree.commitUpdate(recorder);
 
 
     context.logger.info('Installing dependencies');
     context.logger.info('{ng-bootstrap,poper.js,bootstrap} will be added to your package.json');
+    const installTaskId = context.addTask(new NodePackageInstallTask());
     context.addTask(new NodePackageInstallTask({packageName:"@popperjs/core@^2.11.5"}));
     context.addTask(new NodePackageInstallTask({packageName:"bootstrap@5.1.3"}));
     context.addTask(new NodePackageInstallTask({packageName:"@ng-bootstrap/ng-bootstrap@^12.1.2"}));
+    context.addTask(new NodePackageInstallTask({packageName:"@angular/cdk@^13.3.9"}));
+    context.logger.info('Bootstrap.css will be added to your angular.json file')
+    context.addTask(new RunSchematicTask('setup-css',{}),[installTaskId]);
     return tree;
   }
 
---
+++ projects/erg-lib/schematics/ng-add/setup.ts
@@ -0,0 +1,38 @@
+import {chain, Rule } from '@angular-devkit/schematics';
+import { getProjectFromWorkspace } from '@angular/cdk/schematics';
+import { getProjectTargetOptions } from '@angular/cdk/schematics';
+import { updateWorkspace} from '@schematics/angular/utility/workspace';
+
+const themePath = `./node_modules/bootstrap/dist/css/bootstrap.min.css`;
+
+export function cssAdd (options: any): Rule {
+  return async () => {
+    return chain([
+      insertCSSDependency(options.project)
+    ]);
+  };
+}
+
+function insertCSSDependency(project: string): Rule {
+  return chain([
+    addThemeStyleToTarget(project, 'build', themePath),
+    addThemeStyleToTarget(project, 'test', themePath),
+  ]);
+}
+
+function addThemeStyleToTarget(projectName: string, targetName: 'test' | 'build', assetPath: string): Rule {
+  return updateWorkspace(workspace => {
+    const project = getProjectFromWorkspace(workspace, projectName);
+    const targetOptions = getProjectTargetOptions(project, targetName);
+    const styles = targetOptions['styles'] as (string | {input: string})[];
+
+    const existingStyles = styles.map(s => (typeof s === 'string' ? s : s.input));
+
+    for (let [, stylePath] of existingStyles.entries()) {
+      if (stylePath === assetPath)
+        return;
+    }
+
+    styles.unshift(assetPath);
+  });
+}
